a18f37d2eeba73a5947ef1d3f74a8ebea0e7a24b
BertiRean <martinberti02@gmail.com>
2016-06-07 22:58:45 -0400

    Revert "1.1 - General: Creada las Migraciones para las Bases de Datos"
    This reverts commit ec2d2e859165a51f479bbb85f7ccc76d912c03e3.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,14 +12,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('users');
-        
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->timestamps();
         });
     }
 
